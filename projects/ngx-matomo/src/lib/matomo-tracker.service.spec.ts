9d3ae341c3113867ff64c1c4092e3591fdc21d1e
Ambroise Rabier <rabier.ambroise@outlook.fr>
2019-08-04 23:28:14 +0200

    fix tests

--- projects/ngx-matomo/src/lib/matomo-tracker.service.spec.ts
+++ projects/ngx-matomo/src/lib/matomo-tracker.service.spec.ts
@@ -3,7 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { MatomoTracker } from './matomo-tracker.service';
 
 describe('MatomoTracker', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({providers: [MatomoTracker]}));
 
   it('should be created', () => {
     const service: MatomoTracker = TestBed.get(MatomoTracker);
